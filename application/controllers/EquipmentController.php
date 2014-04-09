dae86b0b193f34d90ee2d7ec9d28efeba59667d5
SergeyK <en-karpov-serega@yandex.ru>
2014-04-09 15:17:48 +0300

    fix email

--- application/controllers/EquipmentController.php
+++ application/controllers/EquipmentController.php
@@ -23,7 +23,31 @@ class EquipmentController extends Zend_Controller_Action
             $subject  = "Прайс-лист предоставляемых товаров"; //Тема
             $message  = "Прайс-лист предоставляемых товаров"; //Текст письма
             if ($email != '') {
-                $send->sendEmail($email, $subject, $message, $filename);
+                $to       = $email; //Кому
+                $from     = "info@art-track.ru"; //От кого
+//        $subject  = "Коммерческое предложение"; //Тема
+//        $message  = "Коммерческое предложение"; //Текст письма
+                $boundary = "---"; //Разделитель
+                /* Заголовки */
+                $headers  = "From: $from\nReply-To: $from\n";
+                $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"";
+                $body     = "--$boundary\n";
+                /* Присоединяем текстовое сообщение */
+                $body .= "Content-type: text/html; charset='utf-8'\n";
+                $body .= "Content-Transfer-Encoding: quoted-printablenn";
+                $body .= "Content-Disposition: attachment; filename==?utf-8?B?" . base64_encode($filename) . "?=\n\n";
+                $body .= $message . "\n";
+                $body .= "--$boundary\n";
+                $file     = fopen($filename, "r"); //Открываем файл
+                $text     = fread($file, filesize($filename)); //Считываем весь файл
+                fclose($file); //Закрываем файл
+                /* Добавляем тип содержимого, кодируем текст файла и добавляем в тело письма */
+                $body .= "Content-Type: application/octet-stream; name==?utf-8?B?" . base64_encode($filename) . "?=\n";
+                $body .= "Content-Transfer-Encoding: base64\n";
+                $body .= "Content-Disposition: attachment; filename==?utf-8?B?" . base64_encode($filename) . "?=\n\n";
+                $body .= chunk_split(base64_encode($text)) . "\n";
+                $body .= "--" . $boundary . "--\n";
+                mail($to, $subject, $body, $headers); //Отправляем письмо
             } else {
                 return FALSE;
             }
